822609ae26a95c5074a271a7727751a45408b164
Cicoos <80154981+Cicoos@users.noreply.github.com>
2021-06-11 16:22:26 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+client.config = {
+   // token: 'ODEyMjg2MTg0MTEzNDM4NzQx.YC-idQ.-BqLGv5Wlxdp_s4mN84e1aBvGmY', //token
+    banMembers: ['806581173118697522'], // Ban yetkilileri
+    jailMembers: ['806581173118697522'], // Jail yetkilileri
+    jailRoles: ['806581173118697522'], // Jail rolleri
+    muteMembers: ['807571845464850452'], // Yetkili rolleri
+    muteRoles: ['806258055712866304'], // chat mute rolleri
+    voicemuteRoles: ['806258055712866304'], // ses mute rolleri
+    üstYönetim: ['806581173118697522'], //Üst Yönetim rolleri
+    mesajLog: '', //Mesaj log
+    vipRoles: ['809839955877953576'], //vip rolleri
+    boosterRoles: '811611975439220776', //boosterrolü
+    ekipRoles: ['806581173118697522'], //taglı rolü
+    unregisteres: ['781170112186155028'], //kayıtsız rolü
+    maleRoles: ['781170111691358219'], //erkek rolleri
+    girlRoles: ['781170110920130583'], //kız rolleri
+    mods: ['806581173118697522'], //mod rolleri
+    chat: '810571293383983135', //chat idsi
+    channelID: '810571293383983135', //kayıt kanalı id
+    tag: '1', //tag
+    guildID: '786477463940497449', //sunucu id
+    taglog: '810571293383983135', //tag log id
+
+
+
+    //////
+
+
+
+
+    
+}
